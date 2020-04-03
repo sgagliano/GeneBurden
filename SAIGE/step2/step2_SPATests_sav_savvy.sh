ee2cc5385f7e0d2e66da902909494c59efd25bf1
sgagliano <gsarah6221@gmail.com>
2020-04-03 19:42:17 -0400

    removed start and end parameters

--- SAIGE/step2/step2_SPATests_sav_savvy.sh
+++ SAIGE/step2/step2_SPATests_sav_savvy.sh
@@ -5,19 +5,18 @@
 trait=$1 #X..
 chunkName=$2 #your burden file name before the .Burden.vcf.gz ending
 chrom=$3 #1,2,...22,X
-start=$4
-end=$5
+#start=$4
+#end=$5
 
 Rscript step2_SPATests.R \
         --vcfFile= ${chunkName}.Burden.vcf.gz \
         --vcfFileIndex=${chunkName}.Burden.vcf.gz.tbi \
         --chrom=chr${chrom} \
-        --start=$start \
-        --end=$end \
         --GMMATmodelFile=../step1/output/${trait}.rda \
         --varianceRatioFile=../step1/output/${trait}.varianceRatio.txt \
         --numLinesOutput=1000 \
         --sampleFile=vcfsample.list \
         --SAIGEOutputFile=output/${trait}.${chunkName}.SAIGE.txt \
         --IsOutputAFinCaseCtrl=TRUE
-
+        #--start=$start \
+        #--end=$end \
